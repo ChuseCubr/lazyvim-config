a650aeaffc9b4afb1587e163da1fad2ea0576af7
Chuse <chasevillarroel@gmail.com>
2023-08-22 02:11:49 +0800

    new: colorizer

--- lua/plugins/core/extras.lua
+++ lua/plugins/core/extras.lua
@@ -1,3 +1,14 @@
 return {
   { "godlygeek/tabular" },
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      filetypes = { "*" },
+      user_default_options = {
+        RGB = false,
+        names = false,
+        RRGGBBAA = true,
+      },
+    },
+  },
 }
